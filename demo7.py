2ec2c534814d0be0a1ef51f3f1c376eef80d1c08
张鹏翼 <dividi_z@163.com>
2017-11-16 14:11:04 +0800

    2017年11月16日14:10:58

---
+++ demo7.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Jim'
+# https://www.51nod.com/onlineJudge/questionCode.html#!problemId=1000
+import sys
+print sum([int(i) for i in sys.stdin.readline().split()])
