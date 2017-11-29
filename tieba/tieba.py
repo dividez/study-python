6761f8dcbad0643d37627a5a0447224a71a0ad34
张鹏翼 <dividi_z@163.com>
2017-11-29 17:42:28 +0800

    完善一下

--- tieba/tieba.py
+++ tieba/tieba.py
@@ -43,12 +43,17 @@ class Tool:
 
 
 class BDTB:
-    def __init__(self, baseUrl, seeLZ):
+    def __init__(self, baseUrl, seeLZ, floorTag):
         self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
         self.headers = {'User-Agent': self.user_agent}
         self.baseURL = baseUrl
         self.tool = Tool()
+        self.floor = 1
+        self.file = None
         self.seeLZ = '?see_lz=' + str(seeLZ)
+        self.defaultTitle = u"百度贴吧"
+        # 是否写入楼分隔符的标记
+        self.floorTag = floorTag
 
     def getPage(self, pageNum):
         try:
@@ -63,36 +68,67 @@ class BDTB:
                 print u'连接失败,错误原因' + e.reason
                 return None
 
-    def getTitle(self):
-        page = self.getPage(1)
-        if not page:
-            print '页面获取失败。。。。'
-            return None
+    def getTitle(self, page):
         pattern = re.compile('<h3.*?core_title_txt pull-left text-overflow.*?>(.*?)</h3>', re.S)
         title = re.search(pattern, page)
-        print title.group(1).strip()
+        return title.group(1).strip()
 
-    def getPageNumber(self):
-        page = self.getPage(1)
-        if not page:
-            print '页面获取失败。。。。'
-            return None
+    def getPageNumber(self, page):
         pattern = re.compile('<li.*?l_reply_num.*?<span.*?class="red">(.*?)</span>', re.S)
         pageNumber = re.search(pattern, page)
-        print pageNumber.group(1).strip()
-        self.getContent(page)
+        return pageNumber.group(1).strip()
 
     def getContent(self, page):
         pattern = re.compile('<div.*?id="post_content.*?class="d_post_content j_d_post_content.*?>(.*?)</div>', re.S)
-        content = re.findall(pattern, page)
-        floor = 1
-        for con in content:
-            print floor, u"楼------------------------------------------------------------------------------------------------------------------------------------\n"
-            print self.tool.replace(con)
-            floor += 1
+        items = re.findall(pattern, page)
+        contents = []
+        for item in items:
+            # 将文本进行去除标签处理，同时在前后加入换行符
+            content = "\n" + self.tool.replace(item) + "\n"
+            contents.append(content.encode('utf-8'))
+        return contents
+
+    def setFileTitle(self, title):
+        # 如果标题不是为None，即成功获取到标题
+        if title is not None:
+            self.file = open(title + ".txt", "w+")
+        else:
+            self.file = open(self.defaultTitle + ".txt", "w+")
+
+    def writeData(self, contents):
+        # 向文件写入每一楼的信息
+        for item in contents:
+            if self.floorTag == '1':
+                # 楼之间的分隔符
+                floorLine = "\n" + str(
+                    self.floor) + u"-----------------------------------------------------------------------------------------\n"
+                self.file.write(floorLine)
+            self.file.write(item)
+            self.floor += 1
+
+    def start(self):
+        indexPage = self.getPage(1)
+        pageNum = self.getPageNumber(indexPage)
+        title = self.getTitle(indexPage)
+        self.setFileTitle(title)
+        if pageNum == None:
+            print "URL已失效，请重试"
+            return
+        try:
+            print "该帖子共有" + str(pageNum) + "页"
+            for i in range(1, int(pageNum) + 1):
+                print "正在写入第" + str(i) + "页数据"
+                page = self.getPage(i)
+                contents = self.getContent(page)
+                self.writeData(contents)
+        # 出现写入异常
+        except IOError, e:
+            print "写入异常，原因" + e.message
+        finally:
+            print "写入任务完成"
 
 
 if __name__ == '__main__':
     baseURL = 'http://tieba.baidu.com/p/3138733512'
-    bdtb = BDTB(baseURL, 1)
-    bdtb.getPageNumber()
+    bdtb = BDTB(baseURL, 1, '1')
+    bdtb.start()
