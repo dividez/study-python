0eca50a78eab85cd178ff033f61efdd91062bddc
张鹏翼 <dividi_z@163.com>
2017-11-29 17:15:21 +0800

    2017年11月29日17:15:11 完善代码

--- tieba/tieba.py
+++ tieba/tieba.py
@@ -5,11 +5,41 @@ import urllib2
 __author__ = 'Jim'
 
 
+# 处理页面标签类
+class Tool:
+    # 去除img标签,7位长空格
+    removeImg = re.compile('<img.*?>| {7}|')
+    # 删除超链接标签
+    removeAddr = re.compile('<a.*?>|</a>')
+    # 把换行的标签换为\n
+    replaceLine = re.compile('<tr>|<div>|</div>|</p>')
+    # 将表格制表<td>替换为\t
+    replaceTD = re.compile('<td>')
+    # 把段落开头换为\n加空两格
+    replacePara = re.compile('<p.*?>')
+    # 将换行符或双换行符替换为\n
+    replaceBR = re.compile('<br><br>|<br>')
+    # 将其余标签剔除
+    removeExtraTag = re.compile('<.*?>')
+
+    def replace(self, x):
+        x = re.sub(self.removeImg, "", x)
+        x = re.sub(self.removeAddr, "", x)
+        x = re.sub(self.replaceLine, "\n", x)
+        x = re.sub(self.replaceTD, "\t", x)
+        x = re.sub(self.replacePara, "\n    ", x)
+        x = re.sub(self.replaceBR, "\n", x)
+        x = re.sub(self.removeExtraTag, "", x)
+        # strip()将前后多余内容删除
+        return x.strip()
+
+
 class BDTB:
     def __init__(self, baseUrl, seeLZ):
         self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
         self.headers = {'User-Agent': self.user_agent}
         self.baseURL = baseUrl
+        self.tool = Tool()
         self.seeLZ = '?see_lz=' + str(seeLZ)
 
     def getPage(self, pageNum):
@@ -47,8 +77,11 @@ class BDTB:
     def getContent(self, page):
         pattern = re.compile('<div.*?id="post_content.*?class="d_post_content j_d_post_content.*?>(.*?)</div>', re.S)
         content = re.findall(pattern, page)
+        floor = 1
         for con in content:
-            print con
+            print floor, u"楼------------------------------------------------------------------------------------------------------------------------------------\n"
+            print self.tool.replace(con)
+            floor += 1
 
 
 if __name__ == '__main__':
