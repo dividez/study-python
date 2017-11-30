7befe555d74c96addc6b14d1590830c452bc8a34
张鹏翼 <dividi_z@163.com>
2017-11-30 10:41:23 +0800

    生成词云

--- demo6/weibo.py
+++ demo6/weibo.py
@@ -3,6 +3,12 @@ import codecs
 import csv
 import re
 
+import jieba
+import jieba.analyse
+from matplotlib.image import imread
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 __author__ = 'Jim'
 
 import requests
@@ -11,8 +17,8 @@ import requests
 def wb():
     api = "https://m.weibo.cn/index/my?format=cards&page=%s"
     cookies = dict(
-        cookies_are="_T_WM=05cad175256e0ebe6bd9bfc6cb7c83a5; ALF=1512876008; SCF=AovAf3iaf5_5A-7191mBIHAdrnGPBDSZ3d_2017U8-YlirSDO7lRKzbiE5rUJqENlVBodEni9I_SW1HAQeRQo9E.; SUB=_2A253ATKuDeRhGeNK6lsQ8yrEzTiIHXVUCl7mrDV6PUJbktAKLWTekW0rrGadQ37Paw83FgGOwpYzKoERwA..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5ugm.Doobl8_njRi2vp82X5JpX5K-hUgL.Fo-XeK.pe0BRSoB2dJLoI7DSqg_LxrHXUPiL; SUHB=02MvDN_l_KZ-Ht; SSOLoginState=1510294270")
-    for i in range(1, 30):
+        cookies_are="_T_WM=05cad175256e0ebe6bd9bfc6cb7c83a5; ALF=1514525058; SCF=AovAf3iaf5_5A-7191mBIHAdrnGPBDSZ3d_2017U8-Ylgo9U-V0K9S4h182ldHCZLew-otZrWI01T7rR9vHXKy8.; SUB=_2A253Gy7GDeRhGeNK6lsQ8yrEzTiIHXVU57KOrDV6PUJbktAKLXn9kW1NSVPD_x8NYEAQ_pDt6-luZ6k3UGdC4I1c; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5ugm.Doobl8_njRi2vp82X5JpX5K-hUgL.Fo-XeK.pe0BRSoB2dJLoI7DSqg_LxrHXUPiL; SUHB=0idrYDOg90J9ug; SSOLoginState=1512005271")
+    for i in range(1, 100):
         response = requests.get(url=api % i, params={}, cookies=cookies)
         data = response.json()[0]
         groups = data.get('card_group') or []
@@ -42,12 +48,16 @@ def filter_tags(htmlstr):
     re_br = re.compile('<br\s*?/?>')  # 处理换行
     re_h = re.compile('</?\w+[^>]*>')  # HTML标签
     re_comment = re.compile('<!--[^>]*-->')  # HTML注释
+    re_kuo = re.compile('//@')
+    re_o = re.compile('\[|\]')
     s = re_cdata.sub('', htmlstr)  # 去掉CDATA
     s = re_script.sub('', s)  # 去掉SCRIPT
     s = re_style.sub('', s)  # 去掉style
     s = re_br.sub('\n', s)  # 将br转换为换行
     s = re_h.sub('', s)  # 去掉HTML 标签
     s = re_comment.sub('', s)  # 去掉HTML注释
+    s = re_kuo.sub('', s)
+    s = re_o.sub('', s)
     # 去掉多余的空行
     blank_line = re.compile('\n+')
     s = blank_line.sub('\n', s)
@@ -62,6 +72,35 @@ def write_csv(texts):
             writer.writerow({"text": text})
 
 
-write_csv(wb())
+def read_csv():
+    with codecs.open('./weibo.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            yield row['text']
 
+
+def word_segment(texts):
+    jieba.analyse.set_stop_words("./stop_words.txt")
+    for text in texts:
+        tags = jieba.analyse.extract_tags(text, topK=20)
+        return " ".join(tags)
+
+
+def generate_img(texts):
+    data = " ".join(text for text in texts)
+    mask_img = imread('heart-mask.jpg', format=True)
+    wordcloud = WordCloud(
+        font_path='msyh.ttc',
+        background_color='white',
+        mask=mask_img
+    ).generate(data)
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.savefig('./heart.jpg', dpi=600)
+
+
+
+# generate_img(read_csv())
+# write_csv(wb())
+# word_segment(read_csv())
 # print clearning('Repost')
