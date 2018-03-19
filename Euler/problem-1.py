d769a5f47dea8d55a31be62bc671da18688a10df
张鹏翼 <dividi_z@163.com>
2018-03-19 13:47:55 +0800

    problem 2

--- Euler/problem-1.py
+++ Euler/problem-1.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Jim'
-
+'''
+Problem 1 https://projecteuler.net/problem=1
+'''
 total = 0
 for i in range(1000):
     if ((i % 3 == 0) or (i % 5 == 0)):
        total += i
 
-print total
+print(total)
--- Euler/problem-2.py
+++ Euler/problem-2.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Jim'
+'''
+Problem 2 https://projecteuler.net/problem=2
+'''
+
+a = [1, 2]
+total = 2
+i = 2
+while (1):
+    next = a[i - 1] + a[i - 2]
+    a.append(next)
+    if next % 2 == 0:
+        total = total + next
+    if next > 4000000:
+        break
+    i += 1
+
+print(total)
