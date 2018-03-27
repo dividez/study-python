eb6dc59798914b5f6203cb4a3933b831a6888a8d
张鹏翼 <dividi_z@163.com>
2018-03-27 11:03:52 +0800

    :tada: 素数 10001素数是什么？？？

---
+++ Euler/problem-7.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from math import sqrt
+
+__author__ = 'Jim'
+
+'''
+Problem 7 https://projecteuler.net/problem=7
+
+'''
+
+
+def isprime(num):
+    for i in range(2, int(sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+the_10001st = 0
+for i in range(2, 10 ** 9):
+    the_10001st += isprime(i)
+    if the_10001st == 10001:
+        print(i)
+        break
